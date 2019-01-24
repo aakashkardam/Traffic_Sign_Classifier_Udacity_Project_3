1125202624ef25fabd6f7f6dd542fb6a609c7eef
aakashkardam <aakashkardam94@gmail.com>
2019-01-24 08:35:55 +0000

    Model Architecture, LeNet included in the jupyternotebook. Valid Accuracy = 93.3% achieved.

--- image_augmentation.py
+++ image_augmentation.py
@@ -5,22 +5,16 @@ import skimage as sk
 from skimage import transform
 from skimage import util
 import numpy as np
-<<<<<<< HEAD
 import tensorflow as tf
-=======
->>>>>>> 04194622393591d4d9fc75af55bf9bc135e8632b
 
 def rotate_randomly(img):
   random_degree = random.uniform(-180, 180)
   return sk.transform.rotate(img, random_degree)
 
 def add_noise_randomly(img):
-<<<<<<< HEAD
   #noise = tf.random_normal(shape=tf.shape(img), mean=0.0, stddev=1.0)
   #temp = img+noise
   #tf.reshape(temp,img.shape)
-=======
->>>>>>> 04194622393591d4d9fc75af55bf9bc135e8632b
   return sk.util.random_noise(img)
 
 def flip_upside_down(img):
