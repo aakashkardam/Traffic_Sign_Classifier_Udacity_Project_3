b396de430236678e8c197ef9890af4490bfde6c6
aakashkardam <aakashkardam94@gmail.com>
2019-01-24 05:27:19 +0000

    Augmented data generated and saved as pickle file

--- image_augmentation.py
+++ image_augmentation.py
@@ -5,12 +5,16 @@ import skimage as sk
 from skimage import transform
 from skimage import util
 import numpy as np
+import tensorflow as tf
 
 def rotate_randomly(img):
   random_degree = random.uniform(-180, 180)
   return sk.transform.rotate(img, random_degree)
 
 def add_noise_randomly(img):
+  #noise = tf.random_normal(shape=tf.shape(img), mean=0.0, stddev=1.0)
+  #temp = img+noise
+  #tf.reshape(temp,img.shape)
   return sk.util.random_noise(img)
 
 def flip_upside_down(img):
